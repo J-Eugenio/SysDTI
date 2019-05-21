49eaf68fd5810fe1f74bdb584fc130e51794d22f
J-Eugenio <37482325+J-Eugenio@users.noreply.github.com>
2019-05-21 10:55:42 -0300

    Inicio,Arquivos Usuario

---
+++ dti/controle/usuario/Usuario.controller.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once '../../model/usuario/Usuario.class.php';
+
+    class usuario_controller extends usuario_class{
+        protected $tabela = 'tb_usuario';
+        //CONTINUA
+    }
+?>
